b07e66ea62dd67bbf011092080539b079971dc6b
jmr18c <jmr18c@acu.edu>
2020-11-12 08:16:12 -0600

    Initial Commit

---
+++ Power5FootballStadiums/Power5FootballStadiumsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Power5FootballStadiumsApp.swift
+//  Power5FootballStadiums
+//
+//  Created by Justin Raitz on 11/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Power5FootballStadiumsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
